551be5d29595094ac3b4ea089eb9a58e241dd607
godhanidenis <40867528+godhanidenis@users.noreply.github.com>
2019-11-12 19:27:54 -0800

    Added Web Api

---
+++ SchoolWebApi/SchoolWebApi/Models/DBCtx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolWebApi.Models
+{
+    public class DBCtx : DbContext
+    {
+        public DBCtx(DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Register> RegisterUser { get; set; }
+    }
+    
+}
---
+++ SchoolWebApi/SchoolWebApi/Models/Register.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolWebApi.Models
+{
+    public class Register
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Describe { get; set; }
+        public string Achivement { get; set; }
+        public string Degree { get; set; }
+        public string Experience { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
