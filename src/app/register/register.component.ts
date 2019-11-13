5e4c49eefdb55c1840b635c08658f8f836b9b002
godhanidenis <40867528+godhanidenis@users.noreply.github.com>
2019-11-12 19:25:34 -0800

    Update login system

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,7 +15,13 @@ export class RegisterComponent implements OnInit {
   Gender: string = "male";
   isDeleted: boolean = false;
   date = new FormControl(new Date());
-
+  ChildrenName:any = [];
+  ChildrenAge:any = [];
+  childs = [{"name":"child1","age":"age1"},{"name":"child2","age":"age2"},{"name":"child3","age":"age3"},{"name":"child4","age":"age4"},{"name":"child5","age":"age5"}]
+  file;
+  imgURL;
+  imagePath;
+  message;
   public saveCustomer() {
     /* Typically this method will be used to send the contact form to a server to save it*/
   }
@@ -24,10 +30,31 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+    console.log(inputNode);
+    this.file = inputNode.files[0]
+    console.log(this.file);
+    if (this.file.length === 0)
+      return;
+ 
+    var mimeType = this.file.type;
+    console.log(mimeType);
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    reader.readAsDataURL(this.file); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+      console.log(this.imgURL);
+    }
+  
+  }
   onChange(deviceValue) {
-    alert(this.Category)
-    console.log(deviceValue);
+    this.Category = deviceValue;
 }
 
 }
